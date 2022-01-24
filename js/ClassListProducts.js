0cffe0b26ffc8956a25e0576c8ecdb1a7aabffc8
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-01-24 16:35:49 -0300

    API products working

---
+++ js/ClassListProducts.js
@@ -0,0 +1,14 @@
+class ListProducts {
+
+    static endPoint = "https://kenzie-food-api.herokuapp.com/product";
+
+    static async consumeApi() {
+        const response = await fetch(this.endPoint);
+        const dataProducts = await response.json();
+
+        return dataProducts
+    }
+
+}
+
+export { ListProducts }
