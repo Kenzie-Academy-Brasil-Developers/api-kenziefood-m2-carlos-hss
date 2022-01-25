a5ba65e45dab44eef1c0a0e8b87e429cfd39b89b
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-01-25 10:54:37 -0300

    feat(js): Creating Elements - DOM

--- models/ClassListProducts.js
+++ models/ClassListProducts.js
@@ -1,31 +1,43 @@
-// import { ListProducts } from "./js/ClassListProducts.js"
-
 class CreateListProduct {
 
     static
     createTemplateProduct({ imagem, categoria, nome, descricao, preco }) {
 
-        const section = document.querySelector("section");
+        const section = document.querySelector(".container-products");
 
         const divCard = document.createElement("div");
+        const figure = document.createElemente("figure");
         const img = document.createElement("img");
+        const nameH2 = document.createElement("h2");
         const category = document.createElement("span");
-        const name = document.createElement("h4");
         const description = document.createElement("p");
         const price = document.createElement("span");
 
 
-        divCard.id = "product__card";
+        divCard.className = "product__card";
+        figure.className = "product-figure";
+
+
+        img.className = "product-figure__image";
         img.src = imagem;
+
+        nameH2.className = "product__title";
+        nameH2.innerText = nome;
+
+        category.className = "product-section";
         category.innerText = categoria;
-        name.innerText = nome;
+
+        description.className = "product__description";
         description.innerText = descricao;
+
+        price.className = "container-additional__price";
         price.innerText = preco;
 
 
-        divCard.appendChild(img);
+        figure.appendChild(img);
+        divCard.appendChild(figure);
         divCard.appendChild(category);
-        divCard.appendChild(name);
+        divCard.appendChild(nameH2);
         divCard.appendChild(description);
         divCard.appendChild(price);
 
@@ -33,10 +45,3 @@ class CreateListProduct {
     }
 
 }
-
-
-let itens = JSON.parse(localStorage.getItem("products"))
-console.log(itens)
-itens.forEach((item) => {
-    CreateListProduct.createTemplateProduct(item)
-})
