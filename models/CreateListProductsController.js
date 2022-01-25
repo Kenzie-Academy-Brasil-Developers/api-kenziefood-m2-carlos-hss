a04a960909b3ffb2a26e69119cb1560a4deb1ad7
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-01-25 11:11:19 -0300

    Creating more dynamics in the DOM

--- models/CreateListProductsController.js
+++ models/CreateListProductsController.js
@@ -6,7 +6,8 @@ class CreateListProduct {
         const section = document.querySelector(".container-products");
 
         const divCard = document.createElement("div");
-        const figure = document.createElemente("figure");
+        const figure = document.createElement("figure");
+        const figcaption = document.createElement("figcaption");
         const img = document.createElement("img");
         const nameH2 = document.createElement("h2");
         const category = document.createElement("span");
@@ -17,14 +18,17 @@ class CreateListProduct {
         divCard.className = "product__card";
         figure.className = "product-figure";
 
+        figcaption.classList = "product-figure__figcaption hidden";
+        figcaption.innerText = nome;
 
         img.className = "product-figure__image";
         img.src = imagem;
+        img.alt = nome;
 
         nameH2.className = "product__title";
         nameH2.innerText = nome;
 
-        category.className = "product-section";
+        category.classList = "product-section hidden";
         category.innerText = categoria;
 
         description.className = "product__description";
@@ -35,6 +39,7 @@ class CreateListProduct {
 
 
         figure.appendChild(img);
+        figure.appendChild(figcaption);
         divCard.appendChild(figure);
         divCard.appendChild(category);
         divCard.appendChild(nameH2);
