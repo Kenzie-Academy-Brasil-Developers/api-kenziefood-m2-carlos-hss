c0da2d6ace90b55f8c8d316cf0a7c20f6bb524a7
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-01-25 11:00:48 -0300

    Renaming files js

---
+++ js/ListProductsController.js
@@ -0,0 +1,15 @@
+class ListProducts {
+
+    static endPoint = "https://kenzie-food-api.herokuapp.com/product";
+
+    static async consumeApi() {
+        const response = await fetch(this.endPoint);
+        const dataProducts = await response.json();
+
+        localStorage.products = JSON.stringify(dataProducts);
+
+    }
+
+}
+
+ListProducts.consumeApi();
