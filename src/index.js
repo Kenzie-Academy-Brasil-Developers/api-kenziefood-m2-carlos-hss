121c7b38479a308848b71111b70323c00e65eced
Carlos Henrique <carloshenriquesantanas19@gmail.com>
2022-01-26 19:36:46 -0300

    feat(js): Router Initial Concluded

--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@ import { Cart } from "./controllers/cart.js";
 import { Filter } from "./controllers/filters.js";
 import { CreateListProduct } from "./controllers/CreateListProductsController.js";
 import { ListProducts } from "./controllers/ListProductsController.js";
+import { ProductRouter } from "./router/router.js";
 
 const products = JSON.parse(localStorage.getItem("products"));
 
 if (JSON.parse(localStorage.getItem("cart")).length >= 1) {
-  const sectionCart = document.querySelector(".cart-body");
-  sectionCart.innerHTML = "";
-  Cart.footerValues();
-  for (const i in JSON.parse(localStorage.cart)) {
-    Cart.cartModel(i);
-  }
+    const sectionCart = document.querySelector(".cart-body");
+    sectionCart.innerHTML = "";
+    Cart.footerValues();
+    for (const i in JSON.parse(localStorage.cart)) {
+        Cart.cartModel(i);
+    }
 } else {
-  localStorage.setItem("cart", JSON.stringify([]));
+    localStorage.setItem("cart", JSON.stringify([]));
 }
 
 ListProducts.consumeApi();
 
 products.forEach((product) => {
-  CreateListProduct.createTemplateProduct(product);
+    CreateListProduct.createTemplateProduct(product);
 });
 
 const buttons = Array.from(document.querySelectorAll(".button-menu"));
@@ -29,9 +30,9 @@ const buttonsCart = document.querySelectorAll(".addcart-button");
 buttons[0].addEventListener("click", Filter.buttonAll);
 
 buttons.forEach((button, index) => {
-  if (index > 0 && index < 4) {
-    button.addEventListener("click", Filter.byButton);
-  }
+    if (index > 0 && index < 4) {
+        button.addEventListener("click", Filter.byButton);
+    }
 });
 
 buttons[4].addEventListener("click", Filter.byLowerPrice);
@@ -41,5 +42,5 @@ buttons[5].addEventListener("click", Filter.byHigherPrice);
 inputSearch.addEventListener("input", Filter.byInput);
 
 buttonsCart.forEach((item) => {
-  item.addEventListener("click", Cart.setCartProducts);
+    item.addEventListener("click", Cart.setCartProducts);
 });
---
+++ src/router/router.js
@@ -0,0 +1,56 @@
+class ProductRouter {
+
+    static urlAPI = "https://kenzie-food-api.herokuapp.com/my/product";
+
+    static
+    async post({ nome, preco, categoria, imagem, descricao }) {
+
+        const response = await fetch(this.urlAPI, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjgsImlhdCI6MTY0MzExODc3OSwiZXhwIjoxNjQzOTgyNzc5LCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.cMSKip8ytEcbT3CYBY9_HpYAlZ7diqzhjNghGqPHgVo"
+            },
+            body: JSON.stringify({
+                "nome": nome,
+                "preco": preco,
+                "categoria": categoria,
+                "imagem": imagem,
+                "descricao": descricao
+            })
+        });
+
+        return response;
+    }
+
+    static
+    async patch(object) {
+
+        const response = await fetch(`${this.urlAPI}/:`, {
+            method: "patch",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjgsImlhdCI6MTY0MzExODc3OSwiZXhwIjoxNjQzOTgyNzc5LCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.cMSKip8ytEcbT3CYBY9_HpYAlZ7diqzhjNghGqPHgVo"
+            },
+            body: JSON.stringify(object)
+        });
+
+        return object;
+    }
+
+    static
+    async delete(id) {
+
+        const response = await fetch(`${this.urlAPI}/:${id}`, {
+            method: "delete",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjgsImlhdCI6MTY0MzExODc3OSwiZXhwIjoxNjQzOTgyNzc5LCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.cMSKip8ytEcbT3CYBY9_HpYAlZ7diqzhjNghGqPHgVo"
+            },
+        });
+
+        return id;
+    }
+}
+
+export { ProductRouter }
