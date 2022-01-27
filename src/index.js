8fca7c9faa34f5fff54259572b006148a62eda65
leogm360 <leogm360@gmail.com>
2022-01-27 12:27:31 -0300

    fix(js): Fix Cart Rendering Error

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import { Cart } from "./controllers/cart.js";
 import { Filter } from "./controllers/filters.js";
 import { CreateListProduct } from "./controllers/CreateListProductsController.js";
 import { ListProducts } from "./controllers/ListProductsController.js";
-import { ProductRouter } from "./router/router.js";
 
-if (JSON.parse(localStorage.getItem("cart")) === null) {
-    localStorage.setItem("cart", JSON.stringify([]));
-} else {
-    const sectionCart = document.querySelector(".cart-body");
-    sectionCart.innerHTML = "";
-    Cart.footerValues();
-    for (const i in JSON.parse(localStorage.cart)) {
-        Cart.cartModel(i);
-    }
+if (localStorage.getItem("cart") === "") {
+  localStorage.setItem("cart", JSON.stringify([]));
+} else if (
+  localStorage.getItem("cart") !== "" &&
+  localStorage.getItem("cart") !== "[]"
+) {
+  Cart.footerValues();
+
+  for (const i in JSON.parse(localStorage.cart)) {
+    Cart.cartModel(i);
+  }
 }
 
 ListProducts.consumeApi();
@@ -20,7 +21,7 @@ ListProducts.consumeApi();
 const products = JSON.parse(localStorage.getItem("products"));
 
 products.forEach((product) => {
-    CreateListProduct.createTemplateProduct(product);
+  CreateListProduct.createTemplateProduct(product);
 });
 
 const buttons = Array.from(document.querySelectorAll(".button-menu"));
@@ -29,9 +30,9 @@ const buttonsCart = document.querySelectorAll(".addcart-button");
 
 buttons[0].addEventListener("click", Filter.buttonAll);
 buttons.forEach((button, index) => {
-    if (index > 0 && index < 4) {
-        button.addEventListener("click", Filter.byButton);
-    }
+  if (index > 0 && index < 4) {
+    button.addEventListener("click", Filter.byButton);
+  }
 });
 buttons[4].addEventListener("click", Filter.byLowerPrice);
 buttons[5].addEventListener("click", Filter.byHigherPrice);
@@ -39,5 +40,5 @@ buttons[5].addEventListener("click", Filter.byHigherPrice);
 inputSearch.addEventListener("input", Filter.byInput);
 
 buttonsCart.forEach((item) => {
-    item.addEventListener("click", Cart.setCartProducts);
+  item.addEventListener("click", Cart.setCartProducts);
 });
