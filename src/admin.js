ebd5485015616894472d20d0c7e444f014ceab73
leogm360 <leogm360@gmail.com>
2022-01-27 02:08:12 -0300

    feat(js): Implements Form Change
    - Provide way to change wich form current being displayed.

--- src/admin.js
+++ src/admin.js
@@ -1,7 +1,41 @@
 import { Requisitions } from "./controllers/requisitions.js";
 
 const registerButton = document.querySelector("#register-button");
-const deleteButton = document.querySelector('#delete-button');
+const deleteButton = document.querySelector("#delete-button");
 
-registerButton.addEventListener('click', Requisitions.registerProduct)
-deleteButton.addEventListener('click', Requisitions.deleteProduct)
+registerButton.addEventListener("click", Requisitions.registerProduct);
+deleteButton.addEventListener("click", Requisitions.deleteProduct);
+
+const buttonsMenu = Array.from(document.querySelectorAll(".button-menu"));
+
+function setButtonActive(target) {
+  const lastActive = document.querySelector(".active");
+
+  lastActive.classList.toggle("active");
+  target.classList.toggle("active");
+}
+
+function changeForm(target) {
+  const containerForm = Array.from(
+    document.querySelectorAll(".container-form")
+  );
+  const formActive = document
+    .querySelector(`.${target.value}`)
+    .closest("section");
+
+  containerForm.forEach((form) => {
+    if (!form.className.includes("hidden")) {
+      form.classList.toggle("hidden");
+
+      formActive.classList.toggle("hidden");
+    }
+  });
+}
+
+buttonsMenu.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    setButtonActive(event.target);
+
+    changeForm(event.target);
+  });
+});
