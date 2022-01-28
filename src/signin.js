627a2d438a505ecee62d05cc6ab72873db3c7e39
Carlos Henrique de Santana Santos <90366233+carlos-hss@users.noreply.github.com>
2022-01-28 17:49:12 -0300

    Update signin.js

--- src/signin.js
+++ src/signin.js
@@ -3,9 +3,7 @@ const passwordInput = document.querySelector('#input-password');
 const buttonSignIn = document.querySelector('.button-signin')
 
 function login() {
-    if (userInput.value === 'leogm360' && passwordInput.value === '134679@#Le' ||
-        userInput.value === 'guisix18' && passwordInput.value === 'G2003@g' ||
-        userInput.value === 'carlos-hss' && passwordInput.value === '3465@47') {
+    if (userInput.value === 'admin' && passwordInput.value === 'admin') {
         window.location.assign('./admin.html');
     } else {
         alert('Usuário e/ou senha incorretos.')
